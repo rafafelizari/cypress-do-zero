53b5e6cba04b7d629ca3460d07dc38298f17cd31
ChrisM3llo <mellochris59@gmail.com>
2025-12-19 10:59:06 -0300

    feat: :sparkles: added Hiring acceptance and new config on config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    
+    //timeout padrão de 30 segundos pra tudo
+    defaultCommandTimeout: 30000,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/vincular/desvincular.cy.js
+++ cypress/e2e/vincular/desvincular.cy.js
@@ -2,50 +2,60 @@ Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
 });
 
-describe("Pendencia_Depto_Pessoal", () => {
-  it.only("Desvincular colab pela conta empresa", () => {
+describe.skip("Unlink collaborator", () => {
+  it("Unlink collaborator via company account", () => {
     cy.visit("https://account.inciclebeta.com/");
     cy.get('#root input[type="email"]').type("incicle@incicle.com");
     cy.get('#root input[type="password"]').type("123");
     cy.get('#root button[type="submit"]').click();
 
+
     cy.origin("https://social.inciclebeta.com/", () => {
+
+      //cy.visit("https://social.inciclebeta.com/");
+
       //exceção a erros//
       Cypress.on("uncaught:exception", () => false);
 
-      cy.contains("Feed", { timeout: 10000 }).should("be.visible");
+      cy.contains("Feed", { timeout: 30000 }).should("be.visible");
       cy.wait(1000);
-      cy.get(".incicleheader-desktopmodules-label").click();
-      cy.contains("Departamento Pessoal").click();
-    });
-
-    cy.origin("https://department.inciclebeta.com/", () => {
-      //exceção a erros//
-      Cypress.on("uncaught:exception", () => false);
-      cy.contains("Dashboard", { timeout: 10000 }).should("be.visible");
-      cy.wait(1000);
-      cy.contains("Colaboradores").click();
-      cy.get('[data-cy="InputBase-ActiveCollaborators"]').type(
-        "Erling Haaland"
-      );
-      cy.wait(500);
-      cy.contains("Erling Haaland").click();
-      cy.wait(1000);
-      cy.contains("Desvincular").click();
-      cy.get('[data-cy="TextareaBase"]').type("Teste");
-      cy.contains("Não").click();
-      cy.contains("Desvincular colaborador").click();
-      cy.contains("Confirmar").click();
-      cy.wait(1000);
-      cy.contains("O colaborador foi desvinculado com sucesso").should(
-        "be.visible"
-      );
-    });
+      cy.get(".incicleheader-desktopmodules-label", { timeout: 30000 }).should("be.visible").click();
+    cy.contains("Departamento Pessoal", { timeout: 30000 }).should("be.visible").click();
   });
-});
 
-describe("Contratando colaborador", () => {
-  it("Vincular colab pela conta empresa", () => {
+  cy.origin("https://department.inciclebeta.com/", () => {
+
+    //cy.visit("https://department.inciclebeta.com/");
+
+    //exceção a erros//
+    Cypress.on("uncaught:exception", () => false);
+
+    cy.contains("Dashboard", { timeout: 30000 }).should("be.visible");
+    //cy.wait(1000);
+    cy.contains("Colaboradores", { timeout: 30000 }).should("be.visible").click();
+    cy.get('[data-cy="InputBase-ActiveCollaborators"]', { timeout: 30000 })
+      .should("be.visible")
+      .type("Erling Haaland");
+
+    //cy.wait(500);
+    cy.contains("Erling Haaland", { timeout: 30000 }).should("be.visible").click();
+    //cy.wait(1000);
+    cy.contains("Desvincular").click();
+    cy.get('[data-cy="TextareaBase"]').type("Teste");
+    cy.contains("Não").click();
+    cy.contains("Desvincular colaborador").click();
+    cy.contains("Confirmar").click();
+    cy.wait(1000);
+    cy.contains("O colaborador foi desvinculado com sucesso").should(
+      "be.visible"
+    );
+  });
+
+});
+});
+///////////////////////////////////////////////////////////////////////////////////
+describe.skip("Hire collaborator", () => {
+  it("Hire collaborator via company account", () => {
     cy.visit("https://account.inciclebeta.com/");
     cy.get('#root input[type="email"]').type("incicle@incicle.com");
     cy.get('#root input[type="password"]').type("123");
@@ -55,26 +65,28 @@ describe("Contratando colaborador", () => {
       //exceção a erros//
       Cypress.on("uncaught:exception", () => false);
 
-      cy.contains("Feed", { timeout: 10000 }).should("be.visible");
+      cy.contains("Feed", { timeout: 30000 }).should("be.visible");
       cy.wait(5000);
 
       //Clicando no menu de módulos
-      cy.get(".incicleheader-desktopmodules-label").click();
-      cy.wait(1000);
+      cy.get(".incicleheader-desktopmodules-label" ,{ timeout: 30000 }).should("be.visible").click();
+      //cy.wait(1000);
 
       //Clicando no botão Departamento Pessoal
-      cy.contains("Departamento Pessoal").click();
+      cy.contains("Departamento Pessoal", { timeout: 30000 }).should("be.visible").click();
     });
+
     //Acessando Depto Pessoal
     cy.origin("https://department.inciclebeta.com/", () => {
+
       //exceção a erros//
       Cypress.on("uncaught:exception", () => false);
 
-      cy.contains("Dashboard", { timeout: 10000 }).should("be.visible");
+      cy.contains("Dashboard", { timeout: 30000 }).should("be.visible");
 
-      cy.wait(1000);
+      //cy.wait(1000);
 
-      cy.contains("Colaboradores").click();
+      cy.contains("Colaboradores", { timeout: 30000 }).should("be.visible").click();
       cy.contains("Cadastrar Colaborador").click();
       cy.contains("Adicionar individualmente").click();
       cy.contains("Nome do colaborador")
@@ -90,3 +102,28 @@ describe("Contratando colaborador", () => {
     });
   });
 });
+///////////////////////////////////////////////////////////////////////////////////
+describe.skip("Accept hiring", () => {
+  it("Accept hiring via person account", () => {
+    cy.visit("https://account.inciclebeta.com/");
+    cy.get('#root input[type="email"]').type("samyhyko@fxzig.com");
+    cy.get('#root input[type="password"]').type("1234567890");
+    cy.get('#root button[type="submit"]').click();
+    cy.wait(1000)
+
+    cy.origin("https://social.inciclebeta.com", () => {
+      //exceção a erros//
+      Cypress.on("uncaught:exception", () => false);
+
+      cy.contains("Feed", { timeout: 15000 }).should("be.visible");
+      cy.wait(5000);
+
+      cy.contains("Responda a solicitação de vinculação da empresa").click()
+      cy.wait(5000)
+    });
+
+    cy.contains("Confirmar vínculo").click()
+    cy.contains("Voltar para o sistema").click()
+
+  })
+})
